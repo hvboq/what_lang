86e7232f5c963cf51877aef56f830e89ef1cccd3
hvboq <hvboq11@gmail.com>
2024-06-04 00:16:40 +0900

    [ADD] Quiz Item Delete Feature

--- lib/widgets/quiz_list_item.dart
+++ lib/widgets/quiz_list_item.dart
@@ -5,12 +5,14 @@ class QuizListItem extends StatelessWidget {
   final Quiz inputQuiz;
   final int index;
   final Function onTap;
+  final Function deleteItem;
 
   const QuizListItem(
       {Key? key,
       required this.inputQuiz,
       required this.index,
-      required this.onTap})
+      required this.onTap,
+      required this.deleteItem})
       : super(key: key);
 
   //정렬 한번 더 고민해볼 것
@@ -29,6 +31,29 @@ class QuizListItem extends StatelessWidget {
         style: const TextStyle(color: Colors.grey),
       ),
       onTap: () => {onTap(context, inputQuiz, index)},
+      onLongPress: () => {},
     );
+  }
+
+  Future<void> onLongPress(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: ((context) {
+          return AlertDialog(
+              title: const Text('삭제 확인'),
+              content: Text('${inputQuiz.question} 를 삭제하시겠습니까?'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('취소'),
+                  onPressed: () => {Navigator.pop(context)},
+                ),
+                TextButton(
+                    child: const Text('삭제'),
+                    onPressed: () {
+                      deleteItem(index);
+                      Navigator.of(context).pop();
+                    }),
+              ]);
+        }));
   }
 }
